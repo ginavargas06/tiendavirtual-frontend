9fbf7159fdc38b8faaeb89b929e90c4a3f0f166e
Gina Vargas <ginavargas06@gmail.com>
2021-11-27 19:14:28 -0500

    renombra atributo en producto a costo y precioVenta

--- src/app/producto/lista/producto.ts
+++ src/app/producto/lista/producto.ts
@@ -2,10 +2,10 @@ export class Producto {
   _id!: string;
   categoria!: String;
   nombreProducto!: String;
-  precio!: number;
+  costo!: number;
   proveedor!: String;
   unidad!: number;
   usuario!: String;
   fechaVencimiento!: String;
-  precioUnitario!: number;
+  precioVenta!: number;
 }
